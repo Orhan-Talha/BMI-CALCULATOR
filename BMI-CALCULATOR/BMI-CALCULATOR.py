cc13bd69c1c93dccc6fca5f531d16b535fb6ae6c
Orhan Talha Çetinel <cetinelorhantalha08@gmail.com>
2024-01-18 11:45:15 +0300

    Text and screen resized

--- BMI-CALCULATOR/BMI-CALCULATOR.py
+++ BMI-CALCULATOR/BMI-CALCULATOR.py
@@ -2,21 +2,21 @@ from tkinter import *
 
 window= Tk()
 window.title("BMI CALCULATOR")
-window.minsize(width=300,height=200)
+window.minsize(width=440,height=244)
 
 weight_label=Label(text="ENTER YOUR WEIGHT(KG)",font=44)
-weight_label.pack()
+weight_label.pack(pady=5)
 
-weight_entry=Entry(width=30)
-weight_entry.pack()
+weight_entry=Entry(width=30,font=44)
+weight_entry.pack(pady=5)
 
 height_label=Label(text="ENTER YOUR HEIGHT(CM)",font=44)
-height_label.pack()
+height_label.pack(pady=5)
 
-height_entry=Entry(width=30)
-height_entry.pack()
+height_entry=Entry(width=30,font=44)
+height_entry.pack(pady=5)
 calculate_label=Label(text="____________________",font=44)
-calculate_label.pack()
+calculate_label.pack(pady=5)
 def button_clicked():
     body_mass=""
     try:
@@ -24,9 +24,10 @@ def button_clicked():
         he=int(height_entry.get())
         he=(he/100)**2
         calculate=we/he
+        calculate=round(calculate,2)
     except:
         calculate_label.config(text="Please text a number.",font=44)
-        calculate_label.pack()
+        calculate_label.pack(pady=5)
         return
     if calculate<18.5:
         body_mass="underweight"
@@ -39,8 +40,8 @@ def button_clicked():
     elif 40<=calculate:
         body_mass="morbidly obese"
     calculate_label.config(text="Your BMI ={} You are {}".format(calculate,body_mass),font=44)
-    calculate_label.pack()
+    calculate_label.pack(pady=5)
 button=Button(text="CALCULATE",command=button_clicked,font=44)
-button.pack()
+button.pack(pady=5)
 
 window.mainloop()
