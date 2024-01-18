7e005c25d55ab91dff597f4f93feccb5755cfee5
Orhan Talha Çetinel <cetinelorhantalha08@gmail.com>
2024-01-18 11:22:37 +0300

    BMI Calculator Created

--- BMI-CALCULATOR/BMI-CALCULATOR.py
+++ BMI-CALCULATOR/BMI-CALCULATOR.py
@@ -1,7 +1,7 @@
 from tkinter import *
 
 window= Tk()
-window.title("BMİ CALCULATOR")
+window.title("BMI CALCULATOR")
 window.minsize(width=300,height=200)
 
 weight_label=Label(text="ENTER YOUR WEIGHT(KG)",font=44)
@@ -15,11 +15,30 @@ height_label.pack()
 
 height_entry=Entry(width=30)
 height_entry.pack()
-
 def button_clicked():
-    print("CALCULATED")
-
-button=Button(text="CALCULATE",command=button_clicked)
+    body_mass=""
+    try:
+        we=int(weight_entry.get())
+        he=int(height_entry.get())
+        he=(he/100)**2
+        calculate=we/he
+    except:
+        calculate_label=Label(text="Please text a number.",font=44)
+        calculate_label.pack()
+        return
+    if calculate<18.5:
+        body_mass="underweight"
+    elif 18.5<=calculate<25:
+        body_mass="normal"
+    elif 25<=calculate<30:
+        body_mass="overweight"
+    elif 30<=calculate<40:
+        body_mass="obese"
+    elif 40<=calculate:
+        body_mass="morbidly obese"
+    calculate_label=Label(text="Your BMI ={} You are {}".format(calculate,body_mass),font=44)
+    calculate_label.pack()
+button=Button(text="CALCULATE",command=button_clicked,font=44)
 button.pack()
 
 window.mainloop()
